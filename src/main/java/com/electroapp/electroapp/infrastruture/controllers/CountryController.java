a959c856461a29651a83b9f5aeca7c02750300b5
osman019 <osmanortizrolon@gmail.com>
2025-05-02 17:20:12 -0500

    feat: :sparkles: advance

--- src/main/java/com/electroapp/electroapp/infrastruture/controllers/CountryController.java
+++ src/main/java/com/electroapp/electroapp/infrastruture/controllers/CountryController.java
@@ -1,9 +1,17 @@
 package com.electroapp.electroapp.infrastruture.controllers;
 
+import java.lang.StackWalker.Option;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +30,22 @@ public class CountryController {
     }
     @GetMapping("/{id}")
     public ResponseEntity<?> view(@PathVariable Long id ){
-        
+        Optional<Country> countryOptional = countryService.findById(id);
+        if (countryOptional.isPresent()) {
+            return ResponseEntity.ok(countryOptional.orElseThrow());
+            
+        }
+        return ResponseEntity.notFound().build();
 
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Country coountry ){
+        return ResponseEntity.status(HttpStatus.CREATED).body(countryService.save(coountry));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        Optional<Country> countryOptional = countryService.dele
     }
 }
